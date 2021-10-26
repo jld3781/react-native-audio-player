c758993d993ebaf6eada82a0786f82c02608ce55
Jessica Cantrell <jld3781@gmail.com>
2021-10-26 00:18:40 -0500

    Added all 16 tracks

--- src/tracks.js
+++ src/tracks.js
@@ -4,5 +4,175 @@ export const tracks = [
       url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3',
       title: 'Some Title',
       artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '1',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-2.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '2',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-3.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '3',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-4.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '4',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-5.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '5',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-6.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '6',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-7.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '7',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-8.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '8',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '9',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-10.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '10',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-11.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '11',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-12.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '12',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-13.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '13',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-14.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '14',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-15.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
+    },
+    {
+      id: '15',
+      url: 'https://www.soundhelix.com/examples/mp3/SoundHelix-Song-16.mp3', // Load media from the network
+      title: 'Some Title',
+      artist: 'Some Artist',
+      album: 'Some Album',
+      genre: 'Some Genre',
+      date: '2014-05-20T07:00:00+00:00', // RFC 3339
+      artwork: 'https://picsum.photos/400', // Load artwork from the network
+      duration: 402 // Duration in seconds
     }
   ];
