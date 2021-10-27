dd2e145320882d34078d3b39abae2bccd0b5daa2
Jessica Cantrell <jld3781@gmail.com>
2021-10-26 22:27:58 -0500

    Id should be a string

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
         Capability.Pause
       ],
     });
-    const firstTrackId = 0
+    const firstTrackId = "0"
     const firstTrack = tracks.find(track => track.id === firstTrackId)
     await TrackPlayer.add(firstTrack)
   }
